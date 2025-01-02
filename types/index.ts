21897798e8a3c22df25bbdc8cee8920c2c0ea992
Michael Young <neuromyoung@gmail.com>
2025-01-01 19:01:30 -0500

    minor type fix

--- types/index.ts
+++ types/index.ts
@@ -12,14 +12,12 @@ export type CartItem = z.infer<typeof cartItemSchema>
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>
 export type OrderItem = z.infer<typeof insertOrderItemSchema>
 export type Order = z.infer<typeof insertOrderSchema> & {
-    id: string;
-    createdAt: Date;
-    isPaid: boolean;
-    paidAt: Date | null;
-    isDelivered: boolean;
-    deliveredAt: Date | null;
-    orderItems: OrderItem[];
-    user: { name: string,
-        email: string
-    }
-}
+  id: string;
+  createdAt: Date;
+  isPaid: Boolean;
+  paidAt: Date | null;
+  isDelivered: Boolean;
+  deliveredAt: Date | null;
+  orderItems: OrderItem[];
+  user: { name: string; email: string };
+};
